6b7ff1d2938811081d5b60d0a87f7745e86635df
Apple <apple@Apples-MacBook-Pro-98.local>
2019-07-18 15:39:44 -0500

    first commit

---
+++ motivation.js
@@ -0,0 +1,14 @@
+var button = $("button");
+var input = $("input");
+
+button.on("click", appendInfo);
+
+function appendInfo(){
+  var infoContainer = $(".empty");
+  infoContainer.append(`
+    <div class ="info"
+      <p> ${input.val()}</p>
+    </div>`);
+}
+
+console.log("hello");
