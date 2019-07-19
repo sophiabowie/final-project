2f1b9ba4e60be05b3a7eea4ee44548f45cec2f60
Apple <apple@Apples-MacBook-Pro-98.local>
2019-07-19 10:21:04 -0500

    2nd commit

--- main.js
+++ main.js
@@ -3,13 +3,24 @@ var input = $("input");
 
 button.on("click", appendInfo);
 
+
 function appendInfo(){
   var infoContainer = $(".empty");
   infoContainer.append(`
     <div class ="info"
       <p> ${input.val()}</p>
+      <button class="delete"> Delete </button>
+
     </div>`);
+  $(".info").on("click", ".delete", deleteInfo);
+
+}
+
+  function deleteInfo(event){
+    event.target.parentNode.remove();
   }
+
+
 
 
 
